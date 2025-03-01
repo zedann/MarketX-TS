aef6543cfddde0483fe0250e18df57eff3503bd7
Zedan <zedanmohamedcp@gmail.com>
2025-03-01 14:00:48 +0200

    create authService

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,15 +2,19 @@ import express, { NextFunction, Request, Response } from "express";
 import passport from "passport";
 import AppError from "../utils/appError";
 import admin from "../config/firebase-config";
-import ts from "typescript";
+import {
+  googleGetProfile,
+  googleLogout,
+  verifyOtp,
+} from "../services/authService";
 
 const router = express.Router();
 
+// Google OAuth
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
-
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
@@ -18,45 +22,10 @@ router.get(
     res.redirect(process.env.FRONTEND_URL as string);
   }
 );
+router.get("/profile", googleGetProfile);
 
-router.get("/profile", (req: Request, res: Response) => {
-  if (req.user) {
-    res.json(req.user);
-  } else {
-    res.status(401).json({ error: "Not authenticated" });
-  }
-});
-
-router.get("/logout", (req: Request, res: Response) => {
-  req.logout((err: Error) => {
-    if (err) {
-      return res.status(500).json({ error: "Error logging out" });
-    }
-    res.redirect(process.env.FRONTEND_URL as string);
-  });
-});
+router.get("/logout", googleLogout);
 
 // OTP verification
-
-// @ts-ignore
-router.post(
-  "/verify-otp",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { idToken } = req.body;
-
-    if (!idToken) {
-      return next(new AppError("ID Token is required", 400));
-    }
-    try {
-      const decodedToken = await admin.auth().verifyIdToken(idToken);
-      console.log(`✅ OTP Verified for UID: ${decodedToken.uid}`);
-      res
-        .status(200)
-        .json({ message: "OTP verified successfully", user: decodedToken });
-    } catch (error) {
-      console.error("❌ Error verifying OTP:", error);
-      return next(new AppError("Invalid or expired OTP", 401));
-    }
-  }
-);
+router.post("/verify-otp", verifyOtp);
 export default router;
---
+++ src/services/authService.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../utils/appError";
+import admin from "../config/firebase-config";
+
+export const googleGetProfile = (req: Request, res: Response) => {
+  if (req.user) {
+    res.json(req.user);
+  } else {
+    res.status(401).json({ error: "Not authenticated" });
+  }
+};
+
+export const googleLogout = (req: Request, res: Response) => {
+  req.logout((err: Error) => {
+    if (err) {
+      return res.status(500).json({ error: "Error logging out" });
+    }
+    res.redirect(process.env.FRONTEND_URL as string);
+  });
+};
+
+export const verifyOtp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { idToken } = req.body;
+
+  if (!idToken) {
+    return next(new AppError("ID Token is required", 400));
+  }
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    console.log(`✅ OTP Verified for UID: ${decodedToken.uid}`);
+    res
+      .status(200)
+      .json({ message: "OTP verified successfully", user: decodedToken });
+  } catch (error) {
+    console.error("❌ Error verifying OTP:", error);
+    return next(new AppError("Invalid or expired OTP", 401));
+  }
+};
